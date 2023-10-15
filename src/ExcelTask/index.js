1386e125f8059578ae56fa3ba3210f61f522ec1b
Sarvan.Mammadov <Sarvan.Mammadov@SARVANM-IT>
2023-10-15 14:47:03 +0400

    Third task is done

--- src/ExcelTask/index.js
+++ src/ExcelTask/index.js
@@ -1,43 +1,83 @@
 import * as React from 'react';
 import * as xlsx from 'xlsx';
-// for styles:
-// import 'react-tabulator/lib/styles.css'; // default theme
-// import 'react-tabulator/css/bootstrap/tabulator_bootstrap.min.css'; // use Theme(s)
 import { ReactTabulator, reactFormatter, ReactTabulatorOptions, ColumnDefinition } from 'react-tabulator';
-
 import "react-tabulator/lib/styles.css"; // default theme
 import "react-tabulator/css/bootstrap/tabulator_bootstrap.min.css"; // use Theme(s)
-import DateEditor from "react-tabulator/lib/editors/DateEditor";
-import MultiValueFormatter from "react-tabulator/lib/formatters/MultiValueFormatter";
-import MultiSelectEditor from "react-tabulator/lib/editors/MultiSelectEditor";
-import { createRoot } from 'react-dom/client';
 import { Button, Form, Input, Modal, Select } from "antd";
+import {
+    Chart as ChartJS, ArcElement, CategoryScale,
+    LinearScale,
+    BarElement,
+    Title, Tooltip, Legend
+} from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, CategoryScale,
+    LinearScale,
+    BarElement,
+    Title, Tooltip, Legend);
 
 export default function ExcelTask() {
     const [state, setState] = React.useState({
         data: [],
         selectedName: ''
     });
-    const [data, setdata] = React.useState();
+    const [data, setdata] = React.useState([]);
     const [mapData, setMapData] = React.useState();
     const [isModalOpen, setIsModalOpen] = React.useState(false);
     const [inputValue, setInputValue] = React.useState();
     const [selectValue, setSelectValue] = React.useState();
+    const [showChart1, setShowChart1] = React.useState(false);
+    const [showChart2, setShowChart2] = React.useState(false);
+    const [firstChartData, setFirstChartData] = React.useState({
+        labels: ['Red', 'Blue', 'Green'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(75, 192, 192, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    });
+    const [secondChartData, setSecondChartData] = React.useState({
+        labels: ['Status 0', 'Status 1', 'Status 2'],
+        datasets: [
+            {
+                label: 'Len',
+                data: [],
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    });
+
+
     const editableColumns = [
         { title: 'id', field: 'id', width: 150, headerFilter: 'input' },
         { title: 'len', field: 'len', width: 150, headerFilter: 'input' },
         { title: 'wkt', field: 'wkt', width: 150, headerFilter: 'input' },
         { title: 'status', field: 'status', width: 150, headerFilter: 'input' }
     ];
+
+
+
     const showModal = () => {
         setIsModalOpen(true);
     };
     const handleOk = () => {
         const handleMapData = mapData;
         const handleData = data;
-        handleMapData.set(data[0].id + 1, { id: data[0].id + 1, len: inputValue, status: selectValue })
-        handleData.unshift({ id: data[0].id + 1, len: inputValue, status: selectValue })
+        handleMapData.set(data[0].id + 1, { id: data[0].id + 1, len: inputValue, status: selectValue, wkt: '' })
+        handleData.unshift({ id: data[0].id + 1, len: inputValue, status: selectValue, wkt: '' })
         console.log(handleMapData);
         console.log(handleData);
         setdata([...handleData])
@@ -50,20 +90,6 @@ export default function ExcelTask() {
 
     let ref = React.useRef();
 
-    const rowClick = (e, row) => {
-        console.log('ref table: ', ref.current); // this is the Tabulator table instance
-        // ref?.current && ref?.current.replaceData([])
-        console.log('rowClick id: ${row.getData().id}', row, e);
-        setState({ selectedName: row.getData().name });
-    };
-
-    const clearData = () => {
-        setState({ data: [] });
-    };
-    const modifyData = () => {
-        const _newData = data.filter((item) => item.name === 'Oli Bob');
-        setState({ data: _newData });
-    };
     const readUploadFile = (e) => {
         e.preventDefault();
         if (e.target.files) {
@@ -85,22 +111,100 @@ export default function ExcelTask() {
             reader.readAsArrayBuffer(e.target.files[0]);
         }
     }
-
+    function firstChartSplice(data) {
+        let status0 = 0;
+        let status1 = 0;
+        let status2 = 0;
+        console.log(data);
+        data.forEach((item) => {
+            if (item.status === 0) {
+                status0 += 1;
+            }
+            else if (item.status === 1) {
+                status1 += 1;
+            }
+            else if (item.status === 2) {
+                status2 += 1;
+            }
+        })
+        setFirstChartData({
+            labels: ['Status 0', 'Status 1', 'Status 2'],
+            datasets: [
+                {
+                    label: '# of Votes',
+                    data: [status0, status1, status2],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(75, 192, 192, 1)',
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        })
+        setShowChart1(true);
+    }
+    function secondChartSplice() {
+        let status0Len = 0;
+        let status1Len = 0;
+        let status2Len = 0;
+        data.forEach((item) => {
+            if (item.status === 0) {
+                status0Len += item.len;
+            }
+            else if (item.status === 1) {
+                status1Len += item.len;
+            }
+            else if (item.status === 2) {
+                status2Len += item.len;
+            }
+        })
+        console.log(status1Len);
+        setSecondChartData({
+            labels: ['Status 0', 'Status 1', 'Status 2'],
+            datasets: [
+                {
+                    label: 'Len',
+                    data: [status0Len, status1Len, status2Len],
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+            ],
+        })
+        setShowChart2(true);
+    }
     return (
-        <div style={{ height: '100vh', overflow: 'scroll' }}>
-            <div style={{ display: 'flex' }}>
-                <form>
+        <div>
+
+            {showChart1 && data.length > 0 ? <div style={{ position: 'absolute', top: '10%', right: '5%' }}>
+                <Pie data={firstChartData} />
+            </div> : <></>}
+            {showChart2 && data.length > 0 ? <div style={{ position: 'absolute', top: '55%', right: '5%', height: '30vh' }}>
+                <Bar options={{
+                    'resposive': true, plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                    },
+                }} data={secondChartData} />
+            </div> : <></>}
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+                <Form>
                     <label htmlFor="upload">Upload File</label>
-                    <input
+                    <Input
                         type="file"
                         accept='.xlsx'
                         name="upload"
                         id="upload"
                         onChange={readUploadFile}
                     />
-                </form>
+                </Form>
                 <Button type="primary" onClick={showModal}>
-                    Open Modal
+                    Add New Data
                 </Button>
             </div>
             <Modal title="Baslik" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
@@ -118,12 +222,15 @@ export default function ExcelTask() {
                     />
                 </Form.Item>
             </Modal>
-            <ReactTabulator
-                columns={editableColumns}
-                data={data}
-                footerElement={<span>Footer</span>}
-                options={{ 'selectable': 1 }}
-            />
+            <div style={{ height: '50vh', overflowY: 'scroll' }}>
+                <ReactTabulator
+                    columns={editableColumns}
+                    data={data}
+                    options={{ 'selectable': 1, onclick: () => { console.log('ok') } }}
+                />
+            </div>
+            <div style={{ marginTop: '30px' }}><Button onClick={() => { firstChartSplice(data) }}>Analiz 1</Button><Button onClick={() => secondChartSplice()} style={{ marginLeft: '5px' }}>Analiz 2</Button></div>
+
         </div>
     );
 };
