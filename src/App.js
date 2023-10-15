d31e64bf43d7fcdb7869c87bd2867228012b5a4d
Sarvan.Mammadov <Sarvan.Mammadov@SARVANM-IT>
2023-10-15 15:11:01 +0400

    for deployment

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import "./app.css"
 function App() {
   return (
     <div className="App">
-      {/* <div className='task'>Task1</div>
-      <div className='task'>Task2</div>
-      <div className='task'>Task3</div>
-      <div className='task'>Task4</div> */}
       <ExcelTask />
     </div>
   );
